773a66a72032c015cd40aa4b99a68def5286ba73
A S M Taseen <taseen951753@gmail.com>
2023-07-29 01:03:08 -0400

    linting and formatting

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,19 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { RootState } from "../../app/store";
-import { UserData } from "./types";
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+import { UserData } from './types';
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState: { user: null },
-  reducers: {
-    setCredentials: (state, action) => {
-      const { user } = action.payload;
-      state.user = user;
+    name: 'auth',
+    initialState: { user: null },
+    reducers: {
+        setCredentials: (state, action) => {
+            const { user } = action.payload;
+            state.user = user;
+        },
+        logOut: (state, action) => {
+            state.user = null;
+        },
     },
-    logOut: (state, action) => {
-      state.user = null;
-    },
-  },
 });
 
 export const { setCredentials, logOut } = authSlice.actions;
@@ -21,4 +21,4 @@ export const { setCredentials, logOut } = authSlice.actions;
 export default authSlice.reducer;
 
 export const selectCurrentUser = (state: RootState): UserData | null =>
-  state.auth.user;
+    state.auth.user;
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.scss';
+import reportWebVitals from './reportWebVitals';
 
-import { Provider } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./app/store";
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './app/store';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/*" element={<App />} />
-          </Routes>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            {/* loading should include a Loader component */}
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/*" element={<App />} />
+                    </Routes>
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
